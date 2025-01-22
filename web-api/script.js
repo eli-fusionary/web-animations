ce4f3d586a0690f5e7c2ff61eb38c737a61d71ef
eli-fusionary <eli.lemons@fusionary.com>
2025-01-22 13:42:47 -0500

    Ending lesson code

--- web-api/script.js
+++ web-api/script.js
@@ -1,29 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const gold = "#FFBE00"
-    const element = document.querySelector(".square");
+  const gold = "#FFBE00";
+  const element = document.querySelector(".square");
 
   const squareAnimation = element.animate(
-    {
-        transform: [
-             "translateX(0)",
-             `translateX(calc(100vw - 100px)) rotate(360deg)`
-        ],
-        backgroundColor: [
-            "gold", "blue", "crimson"
-        ],
-        offset: [0, 0.3, 1],
-        easing: ['ease-in', 'linear'],
-        composite: ['add', 'replace', 'add'],
-    },
+    [
+      {
+        transform: "translateX(0)",
+        easing: "ease-in",
+      },
+      {
+        backgroundColor: "blue",
+        offset: 0.8,
+        composite: "replace",
+      },
+      {
+        transform: `translateX(calc(100vw - 100px)) rotate(360deg)`,
+        backgroundColor: "crimson",
+      },
+    ],
     {
       duration: 3000,
       delay: 1000,
-      direction: "alternate",
+      direction: "normal",
       fill: "both",
       iterations: Infinity,
       easing: "linear",
-      timeline: document.timeline
+      composite: "add",
+      iterationComposite: "accumulate",
+      timeline: document.timeline,
     }
   );
-  
 });
