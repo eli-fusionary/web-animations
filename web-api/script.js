c0289b21f976b441763ca38ecc46c2d8a5abf89b
eli-fusionary <eli.lemons@fusionary.com>
2025-01-22 14:29:25 -0500

    Ending lesson code

--- web-api/script.js
+++ web-api/script.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const gold = "#FFBE00";
   const element = document.querySelector(".square");
 
   const squareAnimation = element.animate(
@@ -21,7 +20,7 @@ document.addEventListener("DOMContentLoaded", () => {
     {
       duration: 3000,
       delay: 1000,
-      direction: "normal",
+      direction: "alternate",
       fill: "both",
       iterations: Infinity,
       easing: "linear",
@@ -30,4 +29,42 @@ document.addEventListener("DOMContentLoaded", () => {
       timeline: document.timeline,
     }
   );
+
+  squareAnimation.pause()
+
+  const buttons = document.querySelectorAll('.button');
+
+  buttons.forEach(button => {
+    button.addEventListener('click', () => {
+      if (button.classList.contains('play')) {
+        squareAnimation.play()
+      }
+
+      if (button.classList.contains('pause')) {
+        squareAnimation.pause()
+      }
+
+      if (button.classList.contains('cancel')) {
+        squareAnimation.cancel()
+      }
+
+      if (button.classList.contains('reverse')) {
+        squareAnimation.reverse()
+      }
+
+      if (button.classList.contains('finish')) {
+        squareAnimation.finish()
+      }
+      
+    })
+  })
+
+  const playbackRateInput = document.getElementById('playbackRateInput')
+  const playbackRateInputValue = document.getElementById('playbackRateInputValue')
+
+  playbackRateInput.addEventListener('input', (e) => {
+    squareAnimation.updatePlaybackRate(e.target.value)
+    playbackRateInputValue.value = e.target.value
+  })
+
 });
